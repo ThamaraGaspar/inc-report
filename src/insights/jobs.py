8c146c8bf9376869bb0a6001dff471fe78b805eb
Thamara <silvagaspar92@gmail.com>
2022-12-08 16:19:53 -0300

    requisito 2

--- src/insights/jobs.py
+++ src/insights/jobs.py
@@ -5,39 +5,17 @@ import csv
 
 @lru_cache
 def read(path: str) -> List[Dict]:
-    """Reads a file from a given path and returns its contents
-
-    Parameters
-    ----------
-    path : str
-        Full path to file
-
-    Returns
-    -------
-    list
-        List of rows as dicts
-    """
+   
     with open(path) as file:
         return list(csv.DictReader(file))
 
 
 def get_unique_job_types(path: str) -> List[str]:
-    """Checks all different job types and returns a list of them
-
-    Must call `read`
-
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    list
-        List of unique job types
-    """
-    raise NotImplementedError
-
+    job_types = set()
+    jobs = read(path)
+    for job in jobs:
+        job_types.add(job["job_type"])
+    return job_types
 
 def filter_by_job_type(jobs: List[Dict], job_type: str) -> List[Dict]:
     """Filters a list of jobs by job_type
